ae3c2c5a0bcca3ec76e4046fe561330b1b5d5275
maschuep <55745327+maschuep@users.noreply.github.com>
2022-08-28 19:41:11 +0200

    Update event.service.ts

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -13,9 +13,9 @@ export class EventService {
     static createEventAndNotify(all: MeasurementAttributes[], participant: string) {
         const msg = 'Sie haben vor kurzem mehr Strom ({} Wh) verbraucht. Sie koennen mithilfe der Begleitnachricht erfassen was Sie gemacht haben.';
         const shortMsg = 'Hier klicken und den Stromverbrauch erfassen:\n';
+        
+        //read config from config file
         const sensitivity = Number.parseInt(process.env.EVENT_SENSITIVITY, 10);
-
-
         const backofftimeEvent = Number.parseInt(process.env.EVENT_BACKOFF, 10);
         const backofftimeSMS = Number.parseInt(process.env.SMS_BACKOFF, 10);
 
@@ -27,6 +27,7 @@ export class EventService {
 
                 const latestEvent = avgservice.max(f, d => d.timestamp);
 
+                // use the averageservice to calculate the needed values
                 const usage = avgservice.getUsage(all);
                 const stats = avgservice.average(all);
                 const deviation = usage - stats.avgUsage;
@@ -34,6 +35,8 @@ export class EventService {
                 const offsetEvent = 60000;
 
 
+                // could be inmproved using a guard clause
+                // if a abnormal usage is detected a event is created and the user is notified
                 if (latestEvent < Date.now() - (backofftimeEvent + offsetEvent)
                     && usage > sensitivity * stats.avgUsage && deviationAndStde > 0) {
 
@@ -46,21 +49,23 @@ export class EventService {
                         deviation: stats.stde
                     } as any);
 
+                    // find user and send the sms using the notification service
                     User.findAll({ where: { participant: participant } })
                         .then(users => {
                             const timer = 12000;
                             let count = 1;
 
                             users
+                            // dont send an sms to unkonwn numbers
                             .filter(u => u.phone ? u.phone !== '+41undefined' : false )
                             .forEach(u => {
 
+                                // only send sms if user is active and if the last notification was not recently
                                 if (u.active && u.lastNotification < Date.now() - backofftimeSMS) {
                                     u.lastNotification = Date.now();
                                     u.update(u);
 
-                                    // send link Session
-                                    // problem is the sending, because the encoding changes
+                                    // send link Session for login-free usage of platform
                                     Session.create({ userId: u.userId })
                                         .then(session => {
                                             const tokenId = TokenService.createSmsToken({
@@ -70,7 +75,7 @@ export class EventService {
                                                 userId: u.userId
                                             });
                                             setTimeout(() => {
-
+                                                // send the flash sms after a backoff to avoid having to use a queue on the sms api server
                                                 NotificationService.send({
                                                     message: `${msg.replace('{}', '' + Math.round(usage - stats.avgUsage))}`,
                                                     number: u.phone,
